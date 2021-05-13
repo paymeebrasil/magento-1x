00a64742cf5fe2a8223ea7206c3d092bf67373f8
Junior Maia <comoquepode@gmail.com>
2021-05-13 18:48:22 -0300

    Add lib clipboard

--- skin/frontend/base/default/js/juniormaia/paymee/paymee.js
+++ skin/frontend/base/default/js/juniormaia/paymee/paymee.js
@@ -43,4 +43,19 @@ jQuery(document).ready(function() {
             }
         }, 1000);
     }
+
+    var clipboard = new ClipboardJS('.btn');
+
+    clipboard.on('success', function(e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        alert('Código de pagamento copiado com sucesso!');
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
 });
