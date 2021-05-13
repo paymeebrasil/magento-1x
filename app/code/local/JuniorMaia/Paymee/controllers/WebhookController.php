f66b8732722c22ba8464df5c3182dcd435fbd49f
Junior Maia <comoquepode@gmail.com>
2021-05-13 09:53:11 -0300

    Change layout and webhook returns

--- app/code/local/JuniorMaia/Paymee/controllers/WebhookController.php
+++ app/code/local/JuniorMaia/Paymee/controllers/WebhookController.php
@@ -12,7 +12,9 @@ class JuniorMaia_Paymee_WebhookController extends Mage_Core_Controller_Front_Act
             Mage::helper('juniormaia_paymee')->logs($contents);
 
             $receipt_payload    = json_decode($contents, true);
+            Mage::helper('juniormaia_paymee')->logs($receipt_payload);
             $referenceCode      = $receipt_payload["referenceCode"];
+            $orderComment       = null;
 
             $order = Mage::getModel('sales/order')->loadByIncrementId($referenceCode);
             if(!$order->getId()) {
@@ -20,6 +22,32 @@ class JuniorMaia_Paymee_WebhookController extends Mage_Core_Controller_Front_Act
                 return http_response_code(404);
             }
 
+            if (!array_key_exists('newStatus', $receipt_payload)) {
+                $orderComment = $contents;
+                return false;
+            } else {
+
+                switch ($receipt_payload['newStatus']) {
+                    case 'PAID':
+                        $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true);
+                        $orderComment = "PayMee - Pagamento Aprovado";
+                        break;
+                    case 'CANCELLED':
+                        $orderComment = "PayMee - Pagamento Cancelado";
+                        $this->cancelOrder($order);
+                        break;
+                }
+            }
+
+            $status = Mage::getModel('sales/order_status_history')
+                ->setOrder($order)
+                ->setStatus($order->getStatus())
+                ->setComment($orderComment)
+                ->setEntityName(Mage_Sales_Model_Order::HISTORY_ENTITY_NAME);
+            $order->addStatusHistory($status);
+            $order->save();
+
+            /*
             $paymentStatus = Mage::helper('juniormaia_paymee/api')->checkTransactionStatus(array(
                 'order' => $order,
                 'payload' => $receipt_payload
@@ -40,7 +68,7 @@ class JuniorMaia_Paymee_WebhookController extends Mage_Core_Controller_Front_Act
 
                 var_dump(http_response_code(200));
                 return http_response_code(200);
-            }
+            } */
         }
         catch(Exception $e) {
             print_r($e->getmessage());
@@ -49,5 +77,10 @@ class JuniorMaia_Paymee_WebhookController extends Mage_Core_Controller_Front_Act
         }
     }
 
+    public function cancelOrder($order) {
+        if ($order->canCancel()) {
+            $order->cancel()->save();
+        }
+    }
 
 }
